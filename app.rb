f7489fcb62db13e14c657ac7c153fa92e06aa017
itggot-Noel-Ekbrand <noel.ekbrand@itggot.se>
2018-01-25 10:28:46 +0100

    removed vagrant shitts

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ require 'json'
 		slim(:home)
 	end
 
-	post('/') do
+	post('/') do	
 	username = params["username"]
 	session[:user] = username
 	redirect('/:username')
@@ -17,13 +17,17 @@ require 'json'
 		username = session[:user]
 		league = open("https://euw1.api.riotgames.com/lol/summoner/v3/summoners/by-name/#{username}?api_key=RGAPI-f682ce01-381b-4485-8cc8-8bbd8eb30684")
 		league_data = JSON.parse(league.read)
+		summoner_id = league_data['id']
 		summoner_name = league_data['name']
 		summoner_level = league_data['summonerLevel']
 		summoner_profile = league_data['profileIconId']
-		slim(:index, locals:{ summoner_name:summoner_name, summoner_level:summoner_level, summoner_profile:summoner_profile })
+		league_rank = open("https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/#{summoner_id}?api_key=RGAPI-f682ce01-381b-4485-8cc8-8bbd8eb30684")
+		league_rank_data = JSON.parse(league_rank.read)
+		summoner_tier = league_rank_data['tier']
+		summoner_rank = league_rank_data['rank']
+		slim(:index, locals:{ summoner_name:summoner_name, summoner_level:summoner_level, summoner_profile:summoner_profile, summoner_tier:summoner_tier, summoner_rank:summoner_rank })
 	end
 
 
 end           
-
 
