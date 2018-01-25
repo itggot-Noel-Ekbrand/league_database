1ae2c05e1525d3401269ee2c77bb21f78b48fc4a
itggot-Noel-Ekbrand <noel.ekbrand@itggot.se>
2018-01-25 10:52:00 +0100

    last fix

--- app.rb
+++ app.rb
@@ -23,9 +23,10 @@ require 'json'
 		summoner_profile = league_data['profileIconId']
 		league_rank = open("https://euw1.api.riotgames.com/lol/league/v3/positions/by-summoner/#{summoner_id}?api_key=RGAPI-f682ce01-381b-4485-8cc8-8bbd8eb30684")
 		league_rank_data = JSON.parse(league_rank.read)
+		summoner_wins = league_rank_data['wins']
 		summoner_tier = league_rank_data['tier']
 		summoner_rank = league_rank_data['rank']
-		slim(:index, locals:{ summoner_name:summoner_name, summoner_level:summoner_level, summoner_profile:summoner_profile, summoner_tier:summoner_tier, summoner_rank:summoner_rank })
+		slim(:index, locals:{ summoner_name:summoner_name, summoner_level:summoner_level, summoner_profile:summoner_profile, summoner_tier:summoner_tier, summoner_rank:summoner_rank,summoner_wins:summoner_wins })
 	end
 
 
